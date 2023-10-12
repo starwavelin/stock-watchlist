b3ddd7c17bbf8f2c961ecc3b9e8e84bf24880247
“starwavelin” <benjaminxianlin@gmail.com>
2023-10-12 17:24:53 -0400

    feat: Angular - impl sessionService for store user info in sessionStorate

---
+++ client/src/app/services/session.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * SessionService manages user information (username, email) inside Browser’s Session Storage.
+ */
+
+const USER_KEY = 'auth-user';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SessionService {
+    constructor() {}
+
+    saveUser(user: any): void {
+        window.sessionStorage.removeItem(USER_KEY);
+        window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    }
+
+    getUser(): any {
+        const user = window.sessionStorage.getItem(USER_KEY);
+        return user ? JSON.parse(user) : {};
+    }
+
+    isLoggedIn(): boolean {
+        const user = window.sessionStorage.getItem(USER_KEY);
+        return user ? true : false;
+    }
+
+    // When logout, call the clear()
+    clear(): void {
+        window.sessionStorage.clear();
+    }
+}
